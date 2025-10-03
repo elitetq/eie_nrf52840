b6d2a46aca693d8bf39edb71dde0cd57139002f0
elitetq <jonartbaj@gmail.com>
2025-10-02 19:43:50 -0600

    Made LED Blink 20 times a second

--- app/src/main.c
+++ app/src/main.c
@@ -12,13 +12,14 @@ int main(void) {
         return -1;
     }
 
-    ret = gpio_pin_configure_dt(&led0,GPIO_OUTPUT_ACTIVATE);
+    ret = gpio_pin_configure_dt(&led0,GPIO_OUTPUT_ACTIVE);
     if(ret<0) {
         return ret;
     }
 
     while(1) {
-        
+        gpio_pin_toggle_dt(&led0);
+        k_msleep(50);
     }
     return 0;
 }
