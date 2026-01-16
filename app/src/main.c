5a930d68597774582054a0f16a1adcafc259543a
EliteTQ <jonartbaj@gmail.com>
2026-01-15 20:12:03 -0700

    working on BLE central

--- app/src/main.c
+++ app/src/main.c
@@ -1,94 +1,91 @@
-
-/*
- * main.c
- */
-
+#include <errno.h>
 #include <inttypes.h>
-
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/uuid.h>
 #include <zephyr/kernel.h>
+#include <zephyr/sys/byteorder.h>
 #include <zephyr/sys/printk.h>
-#include <stdint.h>
+#include <zephyr/types.h>
 
-#include "BTN.h"
-#include "LED.h"
+static struct bt_conn* my_connection;
 
-void led_blink(led_id led_n, size_t sleep_time) {
-    LED_set(led_n,1);
-    k_msleep(sleep_time);
-    LED_set(led_n,0);
-    k_msleep(sleep_time);
+static struct bt_uuid_128 BLE_CUSTOM_SERVICE_UUID = 
+  BT_UUID_INIT_128(BT_UUID_128_ENCODE(0x11111111,0x2222,0x3333,0x4444,0x0000'0000'0001));
+  
+static struct bt_uuid_128 BLE_CUSTOM_CHARACTER_UUID =
+  BT_UUID_INIT_128(BT_UUID_128_ENCODE(0x11111111,0x2222,0x3333,0x4444,0x0000'0000'0002));
+
+static void ble_on_device_connected(struct bt_conn* conn, uint8_t err) {
+  if(err != 0) {
+    bt_conn_unref(my_connection);
+    my_connection = NULL;
+  }
+
+  char addr[BT_ADDR_LE_STR_LEN];
+  bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+  printk("Connected: %s\n", addr);
 }
 
+static void ble_on_device_disconnected(struct bt_conn* conn, uint8_t reason) {
+  char addr_my_con[BT_ADDR_LE_STR_LEN];
+  char addr_this_con[BT_ADDR_LE_STR_LEN];
+  char reason_msg = bt_hci_err_to_str(reason);
 
-int8_t first_one(int8_t const *arr,size_t sz) {
-    for(int8_t i = 0; sz > i; i++) {
-        if(arr[i] > 0) {
-            printk("\n");
-            return i;
-        }
-    }
-    return -1;
+  bt_addr_le_to_str(bt_conn_get_dst(my_connection), addr_my_con, sizeof(addr_my_con));
+  bt_addr_le_to_str(bt_conn_get_dst(conn), addr_this_con, sizeof(addr_this_con));
+  if(!strcmp(addr_my_con,addr_this_con)) {
+    bt_conn_unref(conn);
+    conn = NULL;
+    printk("Disconnected: %s\n",reason_msg);
+  }
+
 }
 
-// Important: Assumes pass is a 4 element array of type uint8_t.
-void password(int8_t *pass) {
-    
-    printk("Entering password sequence...\n");
-    int8_t but_arr[] = {0,0,0,0};
-    int8_t f1=-1;
-    bool error = false;
-    for(int i = 0; 4 > i; i++) {
-        while(1) {
-            printk("array: ");
-            for(int j = 0; 4 > j; j++) {
-                but_arr[j] = BTN_check_clear_pressed(j);
-                printk("%d, ",(int)but_arr[j]);
-            }
-            printk("\n");
-            f1 = first_one(but_arr,4);
-            printk("f1 val: %d\n",(int)f1);
-            if(0 <= f1) {
-                led_blink(0,200);
-                if(f1 == pass[i]) {
-                    k_msleep(500);
-                    printk("CORRECT\n");
-                    break;
-                } else {
-                    k_msleep(500);
-                    printk("WRONG?\n");
-                    error = true;
-                    break;
-                }
-            }
-            k_msleep(500);
-        }
-    }
-    if(false == error) {
-        led_blink(0,40);
-        led_blink(1,40);
-        led_blink(2,40);
-        led_blink(3,40);
-    } else {
-        led_blink(0,100);
-        led_blink(0,100);
-        led_blink(0,100);
-        led_blink(0,100);
-    }
+static void ble_on_advertisement_received(const bt_addr_le_t* addr, int8_t rssi, uint8_t type, struct net_buf_simple* ad) {
+  if(my_connection != NULL || (type != BT_GAP_ADV_TYPE_ADV_IND && type != BT_GAP_ADV_TYPE_ADV_DIRECT_IND)) {
+    return;
+  }
+  char name[32] = {0};
+  bt_data_parse(ad,ble_get_adv_device_name_cb,name);
+  printk("Device Name: %s\nDevice MAC Address: %s",name,bt_addr_le_to_str(addr));
+
+  rssi < -50 ? return : bt_le_scan_stop();
+  int ret = bt_conn_le_create(addr,BT_CONN_LE_CREATE_CONN, BT_LE_CONN_PARAM_DEFAULT,&my_connection);
+  if(ret != 0) {
+    bt_conn_unref(my_connection);
+    my_connection = NULL;
+    printk("ERROR: Advertisement found, but connection unsuccessful.\n");
+  }
+  
 }
+
+static bool ble_get_adv_device_name_cb(struct bt_data* data, void* user_data) {
+  char* name = user_data;
+
+  if(data->type == BT_DATA_NAME_COMPLETE || data->type == BT_DATA_NAME_SHORTENED) {
+    memcpy(name,data->data,data->data_len);
+    name[data_len] = 0;
+    return false;
+  }
+  
+  return true;
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+  .connected = ble_on_device_connected,
+  .disconnected = ble_on_device_disconnected,
+};
+
+
 
 
 int main(void) {
-    int8_t pass[] = {1,1,2,2};
-    if(0 > BTN_init()) {
-        return 0;
-    }
-    if(0 > LED_init()) {
-        return 0;
-    }
-    
-    while(1) {
-        password(pass);
-        k_msleep(2000);
-    }
-    return 0;
+  bt_le_scan_start(BT_LE_SCAN_PASSIVE,ble_on_advertisement_received);
+  
 }
