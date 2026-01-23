a14951adacf677cadab98c6b1b7dc3f013d33c1c
EliteTQ <jonartbaj@gmail.com>
2026-01-22 18:49:45 -0700

    Finished lesson9

--- app/src/main.c
+++ app/src/main.c
@@ -25,6 +25,71 @@ static struct bt_uuid_16 discover_uuid = BT_UUID_INIT_16(0);
 static struct bt_gatt_discover_params discover_params;
 static struct bt_gatt_subscribe_params subscribe_params;
 
+static uint8_t notify_func(struct bt_conn* conn, struct bt_gatt_subscribe_params* params, const void* data, uint16_t length) {
+  if(!data) {
+    printk("[UNSUBSCRIBED]\n");
+    params->value_handle = 0U;
+    return BT_GATT_ITER_STOP;
+  }
+  
+  printk("[NOTIFICATION] data %p length %u\n", data, length);
+  for(int i = 0; i < MIN(length, 16); i++) {
+    printk(" 0x%02X", ((uint8_t*)data)[i]);
+  }
+  printk("\n");
+
+  return BT_GATT_ITER_CONTINUE;
+}
+
+static uint8_t discover_func(struct bt_conn* conn, const struct bt_gatt_attr* attr, struct bt_gatt_discover_params* params) {
+  int err;
+
+  if(!attr) {
+    printk("Discover complete\n");
+    (void)memset(params,0,sizeof(*params));
+    return BT_GATT_ITER_STOP;
+  }
+  
+  printk("[ATTRIBUTE] handle %u\n",attr->handle);
+  
+  if(!bt_uuid_cmp(discover_params.uuid, &BLE_CUSTOM_SERVICE_UUID.uuid)) {
+    discover_params.uuid = &BLE_CUSTOM_SERVICE_UUID.uuid;
+    discover_params.start_handle = attr->handle + 1;
+    discover_params.type = BT_GATT_DISCOVER_CHARACTERISTIC;
+
+    err = bt_gatt_discover(conn,&discover_params);
+    if(err) {
+      printk("Discover failed (err %d)\n",err);
+    }
+  } else if(!bt_uuid_cmp(discover_params.uuid,&BLE_CUSTOM_SERVICE_UUID.uuid)) {
+    memcpy(&discover_uuid,BT_UUID_GATT_CCC,sizeof(discover_uuid));
+    discover_params.uuid = &discover_uuid.uuid;
+    discover_params.start_handle = attr->handle + 2;
+    discover_params.type = BT_GATT_DISCOVER_DESCRIPTOR;
+    subscribe_params.value_handle = bt_gatt_attr_value_handle(attr);
+    
+    err = bt_gatt_discover(conn,&discover_params);
+    if(err) {
+      printk("Discover failed (err %d)\n",err);
+    }
+  } else {
+    subscribe_params.notify = notify_func;
+    subscribe_params.value = BT_GATT_CCC_NOTIFY;
+    subscribe_params.ccc_handle = attr->handle;
+    
+    err = bt_gatt_subscribe(conn, &subscribe_params);
+    if(err && err != -EALREADY) {
+      printk("Subscribe failed (%d)\n", err);
+    } else {
+      printk("[SUBSCRIBED]\n");
+    }
+    
+    return BT_GATT_ITER_STOP;
+  }
+  return BT_GATT_ITER_STOP;
+}
+
+
 static void ble_on_device_connected(struct bt_conn* conn, uint8_t err) {
   if(err != 0) {
     bt_conn_unref(my_connection);
@@ -103,59 +168,8 @@ static void ble_on_advertisement_received(const bt_addr_le_t* addr, int8_t rssi,
   
 }
 
-static uint8_t discover_func(struct bt_conn* conn, const struct bt_gatt_attr* attr, struct bt_gatt_discover_params* params) {
-  int err;
 
-  if(!attr) {
-    printk("Discover complete\n");
-    (void)memset(params,0,sizeof(*params));
-    return BT_GATT_ITER_STOP;
-  }
-  
-  printk("[ATTRIBUTE] handle %u\n",attr->handle);
-  
-  if(!bt_uuid_cmp(discover_params.uuid, &BLE_CUSTOM_SERVICE_UUID.uuid)) {
-    discover_params.uuid = &BLE_CUSTOM_SERVICE_UUID.uuid;
-    discover_params.start_handle = attr->handle + 1;
-    discover_params.type = BT_GATT_DISCOVER_CHARACTERISTIC;
 
-    err = bt_gatt_discover(conn,&discover_params);
-    if(err) {
-      printk("Discover failed (err %d)\n",err);
-    }
-  } else if(!bt_uuid_cmp(discover_params.uuid,&BLE_CUSTOM_SERVICE_UUID.uuid)) {
-    memcpy(&discover_uuid,BT_UUID_GATT_CCC,sizeof(discover_uuid));
-    discover_params.uuid = &discover_uuid.uuid;
-    discover_params.start_handle = attr->handle + 2;
-    discover_params.type = BT_GATT_DISCOVER_DESCRIPTOR;
-    subscribe_params.value_handle = bt_gatt_attr_value_handle(attr);
-    
-    err = bt_gatt_discover(conn,&discover_params);
-    if(err) {
-      printk("Discover failed (err %d)\n",err);
-    }
-  } else {
-    subscribe_params.notify = notify_func;
-    subscribe_params.value = BT_GATT_CCC_NOTIFY;
-    subscribe_params.ccc_handle = attr->handle;
-    
-    err = bt_gatt_subscribe(conn, &subscribe_params);
-    if(err && err != -EALREADY) {
-      printk("Subscribe failed (%d)\n", err);
-    } else {
-      printk("[SUBSCRIBED]\n");
-    }
-    
-    return BT_GATT_ITER_STOP;
-  }
-  return BT_GATT_ITER_STOP;
-}
-
-static uint8_t notify_func(struct bt_conn* conn, struct bt_gatt_subscribe_params* params, const void* data, uint16_t length) {
-  if(!data) {
-    printk("[UNSUBSCRIBED]");
-  }
-}
 
 int main(void) {
   k_msleep(5000);
